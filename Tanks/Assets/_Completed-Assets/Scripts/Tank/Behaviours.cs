607e3a7f9d7fe7271a98b41b13ac36180a33c187
Hadzabadza <hadzabadza@yandex.ru>
2017-10-30 19:10:52 +0000

    WIP Movement prediction firing

--- Tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ Tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -10,10 +10,18 @@ namespace Complete
 
     Use this file to specifiy your new behaviour tree.
      */
+
 	public partial class TankAI : MonoBehaviour
 	{
+		private Vector3 lastEnemyPos = new Vector3 (0, 0, 0);
+		public float perceptionUpdateTime=0.05f;
+		public float turnPrecision = 0.05f;
+		private float timeOfLastCheck = 0;
+		private float maxTurn;
+
 		private Root CreateBehaviourTree ()
 		{
+			maxTurn=gameObject.GetComponent<TankMovement>().m_TurnSpeed;
 
 			switch (m_Behaviour) {
 
@@ -42,20 +50,32 @@ namespace Complete
 		}
 		private Node RangedFire ()
 		{
-			return new Action (() => FireRanged ());
+			return new Action (() => CalculatePrediction ());
 		}
 
-		private void FireRanged(){
-			Vector3 targetPos = TargetTransform ().position;
+		private void CalculatePrediction(){
+			Vector3 targetPos = TargetTransform ().position; 
 			Vector3 localPos = this.transform.InverseTransformPoint (targetPos);
-			float requiredVel = Mathf.Sqrt (localPos.magnitude* 15.0f);
-			Debug.Log (requiredVel);
+			Vector3 localDir = this.transform.InverseTransformDirection (targetPos);
+			Vector3 localVel = (targetPos - lastEnemyPos)/(Time.fixedTime-timeOfLastCheck);
+			float DTT = localPos.magnitude; //Distance to target
+			//float requiredSpd = Mathf.Sqrt (DTT* 15.0f); //Very simplified prediction model based on a constant elevated shot start point and a 10 degree angle
+			float requiredSpd = Mathf.Sqrt (DTT * 15.0f) + localVel.magnitude;// * Mathf.Cos (localDir);
+
+			//TODO: add a prediction point ahead of target
+
+
+			Debug.Log ("Speed= " + requiredSpd+ "EnemySpeed= "+localVel.magnitude + "Direction= " +localDir);
+
 			float time = 0.0f;
-			if (requiredVel > 25.0f)
+			if (requiredSpd > 25.0f)
 				time = 1.0f;
-			else if (requiredVel > 15.0f)
-				time = (requiredVel -15.0f)/10.0f;
+			else if (requiredSpd > 15.0f)
+				time = (requiredSpd -15.0f)/10.0f;
 			Fire (time);
+
+			lastEnemyPos = targetPos;
+			timeOfLastCheck=Time.fixedTime;
 		}
 
 		/* Example behaviour trees */
@@ -73,21 +93,23 @@ namespace Complete
 		private Root TrackBehaviour ()
 		{
 			return new Root (
-				new Service (0.2f, UpdatePerception,
+				new Service (perceptionUpdateTime, UpdatePerception,
 					new Selector (
-						new BlackboardCondition ("targetOffCentre",
-							Operator.IS_SMALLER_OR_EQUAL, 0.05f,
-							Stops.IMMEDIATE_RESTART,
+						new BlackboardCondition ("targetOffCentre", Operator.IS_SMALLER_OR_EQUAL, turnPrecision, Stops.IMMEDIATE_RESTART,
                             // Stop turning and fire
 							new Sequence (StopTurning (),
 								RangedFire ())),
-						new BlackboardCondition ("targetOnRight",
-							Operator.IS_EQUAL, true,
-							Stops.IMMEDIATE_RESTART,
-                            // Turn right toward target
-							new Action (() => Turn (0.2f))),
-                            // Turn left toward target
-						new Action (() => Turn (-0.2f))
+						//Checks if the target is to the right 
+						new BlackboardCondition ("targetFarRight", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+							// Turn left toward target
+							new Action (() => Turn (maxTurn))),
+						new BlackboardCondition ("targetFarLeft", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+							// Turn left toward target
+							new Action (() => Turn (-maxTurn))),
+						new BlackboardCondition ("targetOnRight", Operator.IS_EQUAL, true, Stops.IMMEDIATE_RESTART,
+						// Turn left toward target
+							new Action (() => Turn (turnPrecision))),
+						new Action (() => Turn (-turnPrecision))
 					)
 				)
 			);
@@ -101,6 +123,9 @@ namespace Complete
 			blackboard ["targetDistance"] = localPos.magnitude;
 			blackboard ["targetInFront"] = heading.z > 0;
 			blackboard ["targetOnRight"] = heading.x > 0;
+			blackboard ["targetFarRight"] = heading.x > 0.08f;
+			blackboard ["targetFarLeft"] = heading.x < -0.08f;
+
 			blackboard ["targetOffCentre"] = Mathf.Abs (heading.x);
 		}
 	}
